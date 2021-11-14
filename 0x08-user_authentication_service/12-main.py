db02075c6aee38faa947a520da0d339876153576
davidgonzalezfx <dvdramos16@gmail.com>
2021-11-14 18:55:04 -0500

    Feature - 0x08-user_authentication_service

---
+++ 0x08-user_authentication_service/12-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from auth import Auth
+from user import User
+
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+
+
+auth = Auth()
+
+user = auth.register_user("test@test.com", "PwdHashed")
+print(user.id)
+
+session_id = auth.create_session(user.email)
+
+user_from_session = auth.get_user_from_session_id(session_id)
+
+print(user_from_session.id)
---
+++ 0x08-user_authentication_service/README.md
@@ -0,0 +1,14 @@
+# 0x08. User authentication service
+
+## Resources:books:
+Read or watch:
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+---
+
+## Author
+* **David Gonzalez** - [davidgonzalezfx](https://github.com/davidgonzalezfx)
