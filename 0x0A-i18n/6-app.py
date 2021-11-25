1825cfd08939e4df4296270bc027a941f5300ad7
davidgonzalezfx <dvdramos16@gmail.com>
2021-11-25 18:31:49 -0500

    Fix - 0x0A-i18n

--- 0x0A-i18n/6-app.py
+++ 0x0A-i18n/6-app.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python3
-""" Module for trying out Babel i18n """
-from flask_babel import Babel, _
+""" Flask application """
+
+
 from flask import Flask, render_template, request, g
-import pytz
-from typing import Union
+from flask_babel import Babel
 
-app = Flask(__name__, template_folder='templates')
-babel = Babel(app)
-
-
-class Config(object):
-    """ Configuration Class for Babel """
-
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-app.config.from_object(Config)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -27,52 +14,47 @@ users = {
 }
 
 
-def get_user() -> Union[dict, None]:
-    """ Returns a user dictionary or
-    None if the ID cannot be found or
-    if login_as was not passed.
-    """
-
-    try:
-        login_as = request.args.get("login_as")
-        user = users[int(login_as)]
-    except Exception:
-        user = None
-
-    return user
+class Config:
+    """ l18n Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-@app.before_request
-def before_request():
-    """ Operations that happen before any request """
-    user = get_user()
-    g.user = user
+app = Flask(__name__)
+app.config.from_object("1-app.Config")
+babel = Babel(app)
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def hello_world() -> str:
-    """Renders a Basic Template for Babel Implementation"""
+@app.route("/")
+def hello_world():
+    """ Handle default route """
     return render_template("6-index.html")
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """Select a language translation to use for that request"""
-    locale = request.args.get("locale")
-    if locale and locale in app.config['LANGUAGES']:
+def get_locale():
+    """ Gets the best matching language for user """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
         return locale
-
-    if g.user:
-        locale = g.user.get("locale")
-        if locale and locale in app.config['LANGUAGES']:
-            return locale
-
-    locale = request.headers.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        return locale
-
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    if hasattr(g, "user"):
+        if g.user.get('locale') in Config.LANGUAGES:
+            return g.user.get('locale')
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-if __name__ == "__main__":
-    app.run()
+@app.before_request
+def before_request():
+    """ Gets user and set it as a global """
+    user = get_user()
+    if user:
+        g.user = user
+
+
+def get_user():
+    """ Returns user from request """
+    user_id = request.args.get('login_as')
+    if user_id and int(user_id) in users.keys():
+        return users.get(int(user_id))
+    return None
